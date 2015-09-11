a130f7604bf8e573ad678dbefac6dcd95b4f189c
wrathematics <wrathematics@gmail.com>
2015-09-10 20:25:38 -0400

    skeleton  package help  file

---
+++ R/pbdTEST-package.r
@@ -0,0 +1,17 @@
+#' pbd Tests
+#' 
+#' TODO
+#' 
+#' \tabular{ll}{ 
+#'    Package: \tab pbdTEST \cr 
+#'    Type: \tab Package \cr 
+#'    License: \tab BSD 2-clause \cr 
+#'    LazyLoad: \tab yes \cr 
+#' } 
+#' 
+#' @name pbdTEST-package
+#' @docType package
+#' @author Drew Schmidt \email{schmidt AT math.utk.edu}
+#' @references Programming with Big Data in R Website: \url{http://r-pbd.org/}
+#' @keywords Package
+NULL
