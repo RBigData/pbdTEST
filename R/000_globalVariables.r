1063d642a0a735f3191a627c0a1ec0bf9538f99e
wrathematics <wrathematics@gmail.com>
2015-09-10 20:30:19 -0400

    fixed scope false positive

---
+++ R/000_globalVariables.r
@@ -0,0 +1,10 @@
+### This is to avoid the fale positive messages from R CMD check.
+###   "no visible binding for global variable"
+### Suggested by Prof Brian Ripley
+### ?globalVariables
+
+utils::globalVariables(
+  c("stopfun", "nerrors", "catfun", ".__ta", ".__tb", "a", "b", 
+  "printfun", "ntests"
+))
+
